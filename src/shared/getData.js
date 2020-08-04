0a93c8b44bf9f5be445fc70c9b0b36e0abe262da
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2020-08-04 11:35:37 -0300

    Remove unused getData

--- src/shared/getData.js
+++
@@ -1,9 +0,0 @@
-/* global chrome */
-
-async function getData(cid) {
-  return new Promise(resolve => {
-    chrome.storage.local.get(cid, ({ [cid]: data }) => resolve(data));
-  });
-}
-
-export default getData;
