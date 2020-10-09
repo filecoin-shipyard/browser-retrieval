23a75712ca3796e563196cecbc7edca0c776e859
Bruno Leonardo Michels <brunolm7@gmail.com>
2020-10-09 15:23:30 -0300

    Add more proxy retrieval messages

--- src/popup/App/Home/Temp.js
+++ src/popup/App/Home/Temp.js
@@ -1,35 +1,72 @@
 import classNames from 'classnames';
 import React from 'react';
 import socketIO from 'socket.io-client';
-
-import { messages } from '../../services/messages';
 import useOptions from 'src/popup/hooks/useOptions';
+
+import { messageRequestTypes, messageResponseTypes, messages } from '../../services/messages';
+import { sha256 } from '../../services/sha256';
 
 // temp
 const cid = 'bafk2bzacebbhqzi4y546h7gjbduauzha6z33ltequ7hpbvywnttc57xrwcit2';
 
+const handleCidAvailability = (socket) => {
+  socket.on(messageResponseTypes.cidAvailability, (message) => {
+    console.log(messageResponseTypes.cidAvailability);
+    console.log(message);
+
+    if (!message.available) {
+      // TODO: give up
+
+      return;
+    }
+
+    socket.emit(messageRequestTypes.fundsConfirmed, messages.createFundsSent());
+  });
+};
+
+const handleFundsConfirmed = (socket) => {
+  socket.on(messageResponseTypes.fundsConfirmed, (message) => {
+    console.log(messageResponseTypes.fundsConfirmed);
+    console.log(message);
+
+    // TODO: periodically send this message to check on status
+    socket.emit(messageRequestTypes.queryRetrievalStatus, messages.createQueryRetrievalStatus(cid));
+  });
+
+  socket.on(messageResponseTypes.fundsConfirmedErrorInsufficientFunds, () => {
+    // TODO: something
+  });
+  socket.on(messageResponseTypes.fundsConfirmedErrorPriceChanged, () => {
+    // TODO: something
+  });
+};
+
+const handleChunk = (socket) => {
+  socket.on(messageResponseTypes.chunk, (message) => {
+    const dataBuffer = Buffer.from(message.chunk_data, 'base64');
+    const validSha256 = message.chunk_sha256 === sha256(dataBuffer);
+    const validSize = dataBuffer.length === message.chunk_len_bytes;
+
+    if (validSha256 && validSize) {
+      socket.emit(messageRequestTypes.chunkReceived, messages.createChunkReceived(message))
+
+      // TODO: proccess received data
+    } else {
+      socket.emit(messageRequestTypes.chunkReceived, messages.createChunkResend(message))
+    }
+  });
+};
+
 const addHandlers = (socket) => {
-  socket.on('cid_availability', (message) => {
-    console.log('cid_availability');
-    console.log(message);
-
-    socket.emit('funds_confirmed', messages.createFundsSent());
-  });
-
-  socket.on('funds_confirmed', (message) => {
-    console.log('funds_confirmed');
-    console.log(message);
-
-    // TODO: next step
-  });
-  socket.on('funds_confirmed_error_insufficient_funds', () => {});
-  socket.on('funds_confirmed_error_price_changed', () => {});
+  handleCidAvailability(socket);
+  handleFundsConfirmed(socket);
+  handleChunk(socket);
 };
 
 function Temp({ className, ...rest }) {
-  const [options] = useOptions()
+  const [options] = useOptions();
 
-  console.log('options.wsEndpoin', options.wsEndpoin)
+  console.log('options.wsEndpoin', options.wsEndpoin);
 
   const socket = socketIO(options.wsEndpoin);
   addHandlers(socket);
--- src/popup/services/messages.js
+++ src/popup/services/messages.js
@@ -1,7 +1,30 @@
+import { cid } from 'ipld-dag-cbor/src/util';
+
+export const messageRequestTypes = {
+  queryCid: 'query_cid',
+
+  fundsConfirmed: 'funds_confirmed',
+
+  queryRetrievalStatus: 'query_retrieval_status',
+
+  chunkReceived: 'chunk_received',
+  chunkResend: 'chunk_resend',
+};
+
+export const messageResponseTypes = {
+  cidAvailability: 'cid_availability',
+
+  fundsConfirmed: 'funds_confirmed',
+  fundsConfirmedErrorInsufficientFunds: 'funds_confirmed_error_insufficient_funds',
+  fundsConfirmedErrorPriceChanged: 'funds_confirmed_error_price_changed',
+
+  chunk: 'chunk',
+};
+
 export const messages = {
   createGetQueryCID(cid) {
     return {
-      message: 'query_cid',
+      message: messageRequestTypes.queryCid,
       cid,
       miner: 't01352',
     };
@@ -9,9 +32,33 @@ export const messages = {
 
   createFundsSent() {
     return {
-      message: 'funds_confirmed',
+      message: messageRequestTypes.fundsConfirmed,
       client_token: 'HIgP2JW9wHdlTYb89rjEy9/IQDR02EwMvtg4XN5Y/kY=',
       payment_wallet: 'f1stoztiw5sxeyvezjttq5727wfdkooweskpue5fa',
+    };
+  },
+
+  createQueryRetrievalStatus(cid) {
+    return {
+      message: messageRequestTypes.queryRetrievalStatus,
+      cid,
+      client_token: 'HIgP2JW9wHdlTYb89rjEy9/IQDR02EwMvtg4XN5Y/kY=',
+    };
+  },
+
+  createChunkReceived({ cid, id }) {
+    return {
+      message: messageRequestTypes.chunkReceived,
+      cid,
+      id,
+    };
+  },
+
+  createChunkResend({ cid, id }) {
+    return {
+      message: messageRequestTypes.chunkResend,
+      cid,
+      id,
     };
   },
 };
