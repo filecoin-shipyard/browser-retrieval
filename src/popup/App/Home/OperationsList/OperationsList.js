58383c8a8dcdfb4d853f8a87edcc0700bdda671e
Bruno Leonardo Michels <brunolm7@gmail.com>
2020-10-22 15:06:22 -0300

    Add better status; sort by exec time

--- src/popup/App/Home/OperationsList/OperationsList.js
+++ src/popup/App/Home/OperationsList/OperationsList.js
@@ -1,6 +1,6 @@
 /* global chrome */
 import { DateTime } from 'luxon';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from 'src/popup/components/Card';
 import Label from 'src/popup/components/Label';
 import Table from 'src/popup/components/Table';
@@ -9,28 +9,63 @@ import TableRow from 'src/popup/components/TableRow';
 import { operationsQueue } from 'src/shared/OperationsQueue';
 
 const renderStatus = ({ status, invokeAt }) => {
-  if (!status || status === 'waiting') {
-    return DateTime.fromJSDate(invokeAt).diff(DateTime.local()).toFormat('hh:mm:ss');
+  if (!status || ['waiting', 'running'].includes(status)) {
+    const diff = DateTime.fromJSDate(invokeAt).diff(DateTime.local());
+
+    if (diff.as('milliseconds') > 0) {
+      return diff.toFormat('hh:mm:ss');
+    }
+
+    return `running for ${-diff.as('seconds') | 0}s`;
   }
 
   return status;
 };
 
+const dismiss = (operation) => {
+  operationsQueue.remove(operation);
+};
+
+/**
+ * @param {Array.<any>} ops Operations
+ */
 const renderOpsTable = (ops) => {
-  if (!ops) {
-    return <>No operations</>;
+  if (!ops || !ops.length) {
+    return <div className="p-4 py-2">No operations</div>;
   }
 
   return (
     <Table>
       <tbody>
-        {ops.map((op) => (
-          <TableRow key={op.id}>
-            <TableCell width="250px">{op.label}</TableCell>
-            <TableCell width="80px" title={op.invokeAt?.toISOString()}>{renderStatus(op)}</TableCell>
-            <TableCell>{op.output || '-'}</TableCell>
-          </TableRow>
-        ))}
+        {ops
+          .sort((a, b) => {
+            const diff = a.invokeAt - b.invokeAt;
+            if (isNaN(diff)) {
+              return -2;
+            }
+
+            if (!diff) {
+              return 0;
+            }
+
+            return diff > 0 ? 1 : -1;
+          })
+          .map((op) => (
+            <TableRow key={op.id}>
+              <TableCell width="250px">{op.label}</TableCell>
+              <TableCell width="80px" title={op.invokeAt?.toISOString()}>
+                {renderStatus(op)}
+              </TableCell>
+              <TableCell>{op.output || '-'}</TableCell>
+              {['done', 'failed'].includes(op.status) ? (
+                <TableCell>
+                  <button onClick={() => dismiss(op)}>dismiss</button>
+                </TableCell>
+              ) : (
+                <>&nbsp;</>
+              )}
+            </TableRow>
+          ))}
       </tbody>
     </Table>
   );
