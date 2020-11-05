26a9465b40afa6b8c8a44b0d679bcabf1960af25
Bruno Leonardo Michels <brunolm7@gmail.com>
2020-11-05 17:56:26 -0300

    Replace env system

---
+++ src/shared/environments/env.js
@@ -0,0 +1,15 @@
+export const env = {
+  id: 'production',
+  rendezvousIp: 'webrtc-star-1.browser-retrieval.filecoin.io',
+  rendezvousPort: '443',
+  pricesPerByte: { '*': 1000 },
+  wsEndpoint: 'wss://retrievalproxy.browser-retrieval.filecoin.io:443',
+  lotusEndpoint: 'http://3.231.219.184:80/rpc/v0',
+  lotusToken:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.jtLE4n-cWr1lzvFVDj9wupSHqDJgvQFIRP2takFTbAo',
+  paymentInterval: 1048576,
+  paymentIntervalIncrease: 1048576,
+
+  walletPlaceholder: 'f156e3l2vwd5wi5jwdrd6gdg4y7t2yknq6see7xbq',
+  privateKeyPlaceholder: 'ciiFbmF7F7mrVs5E/IT8TV63PdFPLrRs9R/Cc3vri2I=',
+};
--- src/shared/getOptions.js
+++ src/shared/getOptions.js
@@ -1,21 +1,11 @@
 /* global chrome */
+import { env } from 'src/shared/environments/env';
 
 /**
  * Options that cannot be overwritten in Chrome's storage
  */
 const hardCodedOptions = {
-  rendezvousIp: 'webrtc-star-1.browser-retrieval.filecoin.io',
-  rendezvousPort: '443',
-  pricesPerByte: { '*': 1000 },
-  wsEndpoint: 'ws://localhost:3000' || 'wss://retrievalproxy.browser-retrieval.filecoin.io:443',
-  lotusEndpoint: 'http://3.231.219.184:80/rpc/v0',
-  lotusToken:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.jtLE4n-cWr1lzvFVDj9wupSHqDJgvQFIRP2takFTbAo',
-  paymentInterval: 1048576,
-  paymentIntervalIncrease: 1048576,
-
-  walletPlaceholder: 'f156e3l2vwd5wi5jwdrd6gdg4y7t2yknq6see7xbq',
-  privateKeyPlaceholder: 'ciiFbmF7F7mrVs5E/IT8TV63PdFPLrRs9R/Cc3vri2I=',
+  ...env,
 };
 
 /**
